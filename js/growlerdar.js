f15ef38917734b127824e115dc85d1c4a4abc012
Austin Hinderer <zeshon@gmail.com>
2015-06-20 15:29:35 -0700

    Added grunt config

---
+++ js/growlerdar.js
@@ -0,0 +1,10 @@
+/**
+Growlerdar - 0.1.0
+http://growlerdar.com
+Copyright (c) 2015 Austin Hinderer
+License: Apache 2.0
+*/
+// another-script.js for demonstrating file conactenation
+console.log('another-script.js file');
+// main.js
+console.log('main.js file');
