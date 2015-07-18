a5f154489ae7f461e9f5dac475dd9fd83197a0b2
toastynerd <tylermorgan86@gmail.com>
2015-07-17 20:35:12 -0700

    more cleanup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
@@ -18,8 +17,6 @@ var app = express();
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   req.db = db;
